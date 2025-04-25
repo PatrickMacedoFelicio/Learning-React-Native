8e465c47bc15d97201766c086604d746963329f6
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2025-04-25 19:49:32 -0400

    a

--- Learning/screens/WelcomeScreen.js
+++ Learning/screens/WelcomeScreen.js
@@ -32,12 +32,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#121212',
-    justifyContent: 'space-between',
   },
   content: {
+    paddingTop: 80,
     flex: 1,
     padding: 20,
-    justifyContent: 'center',
+
   },
   title: {
     fontSize: 24,
